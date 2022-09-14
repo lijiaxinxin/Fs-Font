28a814199f3888bf50e4468c03e10241e2a935c1
lijiaxinxin <2539631746@qq.com>
2022-09-14 16:27:45 +0800

    dataset

---
+++ build_dataset/my_build_trainset.sh
@@ -0,0 +1,7 @@
+python3 build_meta4train.py \
+--saving_dir ../archive \
+--content_font ../archive/train_font/id_000_simsun.ttc \
+--train_font_dir ../archive/train_font \
+--val_font_dir ../archive/val_font \
+--seen_unis_file ../archive/train_unis.json \
+--unseen_unis_file ../archive/val_unis.json
